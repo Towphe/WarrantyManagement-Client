d15d8c29ef46a82fb087012d3cca8d9468fef66f
Tope <castillochristopherjames@gmail.com>
2023-06-29 15:47:59 +0800

    created relevant dtos

---
+++ src/Model/Data/ProductInfoDto.cs
@@ -0,0 +1,10 @@
+namespace src.Model.Data;
+
+public class ProductInfoDto{
+  public string? ReferenceNumber {get; set;}
+  public string? SalesOrderNumber {get; set;}
+  public int ProductId {get; set;}
+  public string? Variation {get; set;}
+  public DateTime Date {get; set;}
+  public string? ProductDescription {get; set;}
+}
---
+++ src/Model/Data/UserInfoDto.cs
@@ -0,0 +1,8 @@
+namespace src.Model.Data;
+
+public class UserInfoDto{
+  public string? FirstName {get; set;}
+  public string? LastName {get; set;}
+  public string? ContactNumber {get; set;}
+  public string? Email {get; set;}
+}
