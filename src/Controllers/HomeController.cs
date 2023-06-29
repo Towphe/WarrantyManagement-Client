07b3ecc213ef49fcf4567a45f504acca2df8fa9a
Tope <castillochristopherjames@gmail.com>
2023-06-29 15:48:20 +0800

    created main api

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using src.Model.Repo;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Linq;
+using src.Model.Data;
+using src.Service;
+
+[Controller]
+public class HomeController : Controller{
+  public HomeController(WarrantyrepoContext dbCtx){
+    _dbContext = dbCtx;
+  }
+  private WarrantyrepoContext _dbContext {get; set;}
+  public IActionResult Index() => View();
+  [Route("product")]
+  public async Task<IActionResult> GetProductInfo(){
+    // maybe implement in Dapper
+    List<Product> products = await _dbContext.Products.ToListAsync();
+    ViewBag.ProductCount = products.Count;
+    return View(products);
+  }
+  [Route("user")]
+  public async Task<IActionResult> GetUserInfo(ProductInfoDto productInfo){
+    TempData.Put<ProductInfoDto>("product", productInfo);
+    return View();
+  }
+  [Route("finish")]
+  public async Task<IActionResult> Finish(UserInfoDto userInfoDto){
+    ProductInfoDto productInfo = TempData.Get<ProductInfoDto>("product");
+    Entry entry = new Entry(){
+      Id = IDGenerator.GenerateID("ENT"),
+      FirstName = userInfoDto.FirstName,
+      LastName = userInfoDto.LastName,
+      ContactNumber = userInfoDto.ContactNumber,
+      Email = userInfoDto.Email,
+      ProductId = productInfo.ProductId,
+      ReferenceNum = productInfo.ReferenceNumber,
+      SalesOrderNum = productInfo.SalesOrderNumber,
+      Variation = productInfo.Variation,
+      DatePurchased = DateOnly.FromDateTime(productInfo.Date),
+      DateAdded = DateOnly.FromDateTime(DateTime.Now)
+    };
+    await _dbContext.Entries.AddAsync(entry);
+    await _dbContext.SaveChangesAsync();
+
+    return View(userInfoDto);
+  }
+}
