bae49882f21de21a38a2486f0dcb0581e90c368e
Tope <castillochristopherjames@gmail.com>
2023-06-30 15:39:44 +0800

    updated product info dto

--- src/Model/Data/ProductInfoDto.cs
+++ src/Model/Data/ProductInfoDto.cs
@@ -4,6 +4,7 @@ public class ProductInfoDto{
   public string? ReferenceNumber {get; set;}
   public string? SalesOrderNumber {get; set;}
   public int ProductId {get; set;}
+  public string? StoreName {get; set;}
   public string? Variation {get; set;}
   public DateTime Date {get; set;}
   public string? ProductDescription {get; set;}
