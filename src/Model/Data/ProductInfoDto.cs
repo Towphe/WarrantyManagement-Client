ba55752c05917efe041edf5ca334d4f70c47ec15
Tope <castillochristopherjames@gmail.com>
2023-07-17 12:15:00 +0800

    added media to ProductInfoDto

--- src/Model/Data/ProductInfoDto.cs
+++ src/Model/Data/ProductInfoDto.cs
@@ -3,6 +3,7 @@ namespace src.Model.Data;
 public class ProductInfoDto{
   public string? ReferenceNumber {get; set;}
   public string? SalesOrderNumber {get; set;}
+  public List<IFormFile>? Media {get; set;}
   public int ProductId {get; set;}
   public string? StoreName {get; set;}
   public string? Variation {get; set;}
