a33b21e57aa03954c389a16e466db93787aa62e8
Tope <castillochristopherjames@gmail.com>
2023-07-17 12:16:02 +0800

    added support for media upload"

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -6,20 +6,29 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using System.Linq;
 using src.Model.Data;
 using src.Service;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 [Controller]
 public class HomeController : Controller{
   public HomeController(WarrantyrepoContext dbCtx){
     _dbContext = dbCtx;
   }
+  private string _directory = @"C:\ApplicationData\WarrantyManagement\Temp\Uploads";
   private WarrantyrepoContext _dbContext {get; set;}
   public IActionResult Index(){
     ViewData["Title"] = "Warranty";
     return View();
   }
+  [Route("user")]
+  public async Task<IActionResult> GetUserInfo(){
+    ViewData["Title"] = "User Info";
+    return View();
+  }
   [Route("product")]
-  public async Task<IActionResult> GetProductInfo(){
+  public async Task<IActionResult> GetProductInfo(UserInfoDto userInfo){
     // maybe implement in Dapper
+    TempData.Put<UserInfoDto>("userInfo", userInfo);
     List<Product> products = await _dbContext.Products.ToListAsync();
     List<Merchant> merchants = await _dbContext.Merchants.ToListAsync();
     ViewBag.ProductCount = products.Count;
@@ -27,33 +36,43 @@ public class HomeController : Controller{
     ViewData["Title"] = "Product Info";
     return View(products);
   }
-  [Route("user")]
-  public async Task<IActionResult> GetUserInfo(ProductInfoDto productInfo){
-    TempData.Put<ProductInfoDto>("product", productInfo);
-    ViewData["Title"] = "User Info";
-    return View();
-  }
   [Route("finish")]
-  public async Task<IActionResult> Finish(UserInfoDto userInfoDto){
-    ProductInfoDto productInfo = TempData.Get<ProductInfoDto>("product");
+  public async Task<IActionResult> Finish(ProductInfoDto productInfo){
+    UserInfoDto userInfo = TempData.Get<UserInfoDto>("userInfo");
+    string entryId = IDGenerator.GenerateID("ENT");
     Entry entry = new Entry(){
-      Id = IDGenerator.GenerateID("ENT"),
-      FirstName = userInfoDto.FirstName,
-      LastName = userInfoDto.LastName,
-      ContactNumber = userInfoDto.ContactNumber,  
-      Email = userInfoDto.Email,
+      Id = entryId,
+      FirstName = userInfo.FirstName,
+      LastName = userInfo.LastName,
+      ContactNumber = userInfo.ContactNumber,  
+      Email = userInfo.Email,
       ProductId = productInfo.ProductId,
       ReferenceNum = productInfo.ReferenceNumber,
       SalesOrderNum = productInfo.SalesOrderNumber,
       Variation = productInfo.Variation,
       Store = productInfo.StoreName,
       Status = "UNRESOLVED",
+      Description = productInfo.ProductDescription,
       DatePurchased = DateOnly.FromDateTime(productInfo.Date),
       DateAdded = DateOnly.FromDateTime(DateTime.Now)
     };
+    // save media here
+
+    var client = new AmazonS3Client();
+    string bucketName = "warranty-media";
+    int mediaCount = 1;
+    foreach(FormFile file in productInfo.Media){
+      var objectRequest = new PutObjectRequest(){
+        BucketName = bucketName,
+        Key = $"{entryId}/{mediaCount}",
+        InputStream= file.OpenReadStream()
+      };
+      var response = await client.PutObjectAsync(objectRequest);  
+      mediaCount += 1;
+    }
     await _dbContext.Entries.AddAsync(entry);
     await _dbContext.SaveChangesAsync();
     ViewData["Title"] = "Request Submitted";
-    return View(userInfoDto);
+    return View(userInfo);
   }
 }
