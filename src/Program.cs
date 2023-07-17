00e83aec8f7c249117fd81a48f08751c312a954c
Tope <castillochristopherjames@gmail.com>
2023-07-17 12:14:45 +0800

    configured startup and added packages

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using src.Model.Repo;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,7 +28,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
-app.UseSession();
 app.MapDefaultControllerRoute();
-
+app.UseSession();
 app.Run();
